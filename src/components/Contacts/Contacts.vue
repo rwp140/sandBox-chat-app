f231b04abda5640010cc0f43362dbebad999b32f
Ross William Parsons [140] <rwp140@gmail.com>
2017-12-29 18:26:27 -0700

    established contacts component

---
+++ src/components/Contacts/Contacts.vue
@@ -0,0 +1,17 @@
+<template src="./contacts.template.html">
+</template>
+
+<script>
+export default {
+  name: 'Contacts',
+  data () {
+    return {
+      msg: 'Welcome to SignIn'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style src='./Contacts.style.css' scoped>
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld.vue'
 import SignIn from '@/components/SignIn/SignIn.vue'
+import Contacts from '@/components/Contacts/Contacts.vue'
 
 Vue.use(Router)
 
@@ -16,6 +17,11 @@ export default new Router({
       path: '/',
       name: 'SignIn',
       component: SignIn
+    },
+    {
+      path: '/Contacts',
+      name: 'Contacts',
+      component: Contacts
     }
 
   ]
