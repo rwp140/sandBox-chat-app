f2b1ebe811e793ac38481e537c0b65b5caeefa69
Ross William Parsons [140] <rwp140@gmail.com>
2018-01-06 21:45:01 -0700

    controller set up [signIN.Controller]

--- src/components/SignIn/SignIn.Controller.js
+++ src/components/SignIn/SignIn.Controller.js
@@ -1,8 +1,45 @@
-export default {
+//Imports
+
+
+}
+//Exports
+module.exports = {
   name: 'SignIn',
   data () {
     return {
       msg: 'Welcome to SignIn'
     }
-  }
+  },
+  methods: {
+    LogIN: LogIN,
+    OpenForgotUserForm:OpenForgotUserForm,
+    OpenNewUserForm:OpenNewUserForm
+  },
+  components: {}
 }
+//Functions
+
+  //public
+  function LogIN(){
+
+  }
+  function OpenForgotUserForm(){
+
+  }
+  function OpenNewUserForm(){
+
+  }
+  //Private
+  function buttonCheck(callBack,event){
+
+    event.preventdefault();
+    callBack()
+  }
+  function EncryptPass(){
+
+  }
+  function InputValidation(){
+
+  }
+
+//Event listeners
