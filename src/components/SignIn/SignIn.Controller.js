b0aeb8e9485ddb8e27a08471deb278a63eae9310
Ross Parsons <rwp140@gmail.com>
2018-01-07 04:05:20 -0700

    updated to log in function [sign in controller]

--- src/components/SignIn/SignIn.Controller.js
+++ src/components/SignIn/SignIn.Controller.js
@@ -1,13 +1,15 @@
 //Imports
 
 
-}
+
 //Exports
 module.exports = {
   name: 'SignIn',
   data () {
     return {
-      msg: 'Welcome to SignIn'
+      msg: 'Welcome to SignIn',//used for testing generally
+      username:username,
+      password:password
     }
   },
   methods: {
@@ -19,6 +21,10 @@ module.exports = {
 }
 // Variables
   //public
+  var username = "";
+  var password = "";
+  //private
+
 //Functions
 
   //public
@@ -27,9 +33,11 @@ module.exports = {
   */
   function LogIN(event){
      buttonCheck(()=>{
-
+       if(FormValidation())
+        if(InputValidation())
      },event);
   }
+
   function OpenForgotUserForm(event){
      buttonCheck(()=>{
 
@@ -41,6 +49,10 @@ module.exports = {
      },event);
   }
   //Private
+  /*
+  * Sets up anything needed on mobile platform, disables default event, and runs
+  * any algorythm we make to determin the user genuinly pressed the button
+  */
   function buttonCheck(callBack,event){
 
     event.preventdefault();
@@ -49,11 +61,30 @@ module.exports = {
   function EncryptPass(){
 
   }
-  function InputValidation(){
   /*
   * Runs any form validation we need
   */
+  function FormValidation(){
+    var bool = true;
 
+
+    return bool;
+  }
+  /*
+  * Runs any data validation we need
+  */
+  function InputValidation(IllegalChars,string){
+    var bool = true;
+
+    for(let i =0,l = IllegalChars.length;(i<l && bool==true);i++){
+      let illegalChar = IllegalChars[i];
+      for(let j=0,c=string.length;(j<c&&bool==true);j++){
+        let char = string[j];
+        if(char == illegalChar)bool == false
+      }
+    }
+
+    return bool;
   }
 
 //Event listeners
