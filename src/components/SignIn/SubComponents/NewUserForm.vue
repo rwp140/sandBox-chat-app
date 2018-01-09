d0d2229902c8fdcdc5e8b66599b43a124d8af0fb
Ross Parsons <rwp140@gmail.com>
2018-01-08 21:43:55 -0700

    basic set up for log in pop ups

---
+++ src/components/SignIn/SubComponents/NewUserForm.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="Pop-Up">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: '',
+  data () {
+    return {
+      msg: 'Pop-Up'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.Pop-Up{
+
+}
+</style>
