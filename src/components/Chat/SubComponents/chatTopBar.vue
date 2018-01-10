9a239da8e1f8bedf4f1ed602164b1e2bf05ad9ad
Ross Parsons <rwp140@gmail.com>
2018-01-10 16:19:40 -0700

    updated chat top bar

--- src/components/Chat/SubComponents/chatTopBar.vue
+++ src/components/Chat/SubComponents/chatTopBar.vue
@@ -8,7 +8,6 @@ export default {
   name: 'ChatTopBar',
   data () {
     return {
-      msg: ''
     }
   }
 }
@@ -16,4 +15,10 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+div{
+  background-color: lightblue;
+  max-width:50%;
+  text-align: left;
+  padding: 1%;
+}
 </style>
