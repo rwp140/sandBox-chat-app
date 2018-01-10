4731c86058ede58756d23f5395a863993005ad91
Ross Parsons <rwp140@gmail.com>
2018-01-10 02:08:14 -0700

    this makes sure sub compoents hot reload, if hot reloading breaks check here

--- build/vue-loader.conf.js
+++ build/vue-loader.conf.js
@@ -11,6 +11,7 @@ module.exports = {
     sourceMap: sourceMapEnabled,
     extract: isProduction
   }),
+  hotReload: true,
   cssSourceMap: sourceMapEnabled,
   cacheBusting: config.dev.cacheBusting,
   transformToRequire: {
