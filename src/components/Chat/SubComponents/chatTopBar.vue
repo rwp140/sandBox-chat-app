c689e34a4f5ce2893211bc6e396db35fd5df6558
Ross William Parsons [140] <rwp140@gmail.com>
2018-01-09 18:19:21 -0700

    basic set up for chat sub components

---
+++ src/components/Chat/SubComponents/chatTopBar.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ChatTopBar',
+  data () {
+    return {
+      msg: ''
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
