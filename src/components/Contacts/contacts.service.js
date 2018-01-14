b21c4e8c4005d5b7077411dd5de0310d31a23e22
Ross Parsons <rwp140@gmail.com>
2018-01-13 20:02:16 -0700

    add magic show test code [contacts service]

--- src/components/Contacts/contacts.service.js
+++ src/components/Contacts/contacts.service.js
@@ -1,5 +1,6 @@
 //imports
-const Router = require('../.././router/index').default;
+// const Router = require('../.././router/index').default;
+const store = require('../../store');
 
 //exports
 // module.exports = {
@@ -14,11 +15,23 @@ function test() {
 //Varaibles
   //Public
   //Private
+  var dummyData= {chatID:11992, messages:[{SenderName:"Lena",content:"Hello Ann."},{SenderName:"Anna",content:"Hello Lena!"}]};
 //Functions
   //Public
   function LoadChat(rotuer){
     console.log("go");
     // console.log(Router);
+    // store.state.ChatID = 11992;
+    let ID = 11992;
+    store.state.chatID = ID
+    console.log(ID,store.state.chatID);
+    //route
+    if(ID==dummyData.chatID){
+      console.log(dummyData.messages);
+      store.state.messages= dummyData.messages;
+      console.log(store.state.messages);
+    }
+
     rotuer.push({ path: '/chat' });
   }
   function RENDERContactsListGET(){
