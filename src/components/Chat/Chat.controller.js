7b5808b408b7f5bdffa1b9a54e68302f746ed67e
Ross Parsons <rwp140@gmail.com>
2018-01-14 21:56:09 -0700

    updated message loading interactions

--- src/components/Chat/Chat.controller.js
+++ src/components/Chat/Chat.controller.js
@@ -9,21 +9,33 @@ const svc = require('./Chat.service.js')
 module.exports = {
   name: 'Chat',
   data () {
-    return {
-      chatID: store.state.chatID, //'Welcome to Chat'
-      messages:store.state.messages
-    }
+    return data
   },
-  components:{MessageItem:MessageItem,ActionSelection:actionSelection,ChatTopBar:chatTopBar}
+  components:{
+    MessageItem:MessageItem,
+    ActionSelection:actionSelection,
+    ChatTopBar:chatTopBar
+  },
+  mounted:LoadChat
 }
-console.log(store);
-console.log(store.state);
+// console.log(store);
+// console.log(store.state);
 //Variables
   //public
+  var data = {};
+  data.chatID = 0;
+  data.messages = {};
   //private
 //Function
   //public
   function LoadChat(){
-
+    // console.log("loading chat");
+    // let chatID = store.state.chatID;
+    //this.$nextTick(function(){
+      data.chatID = store.state.chatID;
+      // console.log("chatID", data.chatID,store.state.chatID);
+      // console.log("chatID", data.chatID,store.state.chatID);
+      data.messages = svc.LoadChat(data.chatID);
+    // })
   }
   //private
--- src/components/Chat/Chat.service.js
+++ src/components/Chat/Chat.service.js
@@ -2,17 +2,26 @@
 const store = require('../../store');
 
 //Exports
-
+module.exports ={
+  LoadChat
+}
 //Variables
   //public
   //private
-  // var dummyData= {chatID:11992, messages:[{SenderName:"Lena",content:"Hello Ann."}]};
+  var dummyData= {
+    chatID:11992,
+    messages:[
+      {SenderName:"Lena",content:"Hello Ann.",direction:"right"},
+      {SenderName:"Anna",content:"Hello Lena!",direction:"left"}
+    ]
+  };
 //Functions
   //Public
   function LoadChat(ID){
     //route
-    // if(ID==dummyData.chatID){
-    //   return dummyData.messages;
-    // }
+    if(ID==dummyData.chatID){
+      console.log(dummyData);
+      return dummyData.messages;
+    }
   }
   //Private
