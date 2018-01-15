c6066e4af1297114b4aab27316fbf87d81ef133f
Ross Parsons <rwp140@gmail.com>
2018-01-14 23:25:25 -0700

    updated chat top bar

--- src/components/Chat/SubComponents/chatTopBar.vue
+++ src/components/Chat/SubComponents/chatTopBar.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="TopBar">
-    <span><</span> <h2>Contacts</h2> <span>{{recipient}}</span>
+    <span><-</span> <span>back</span> <span>{{recipient}}</span>
 
   </div>
 </template>
