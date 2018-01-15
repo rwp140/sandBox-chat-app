b8911c298f8266eb04a5635363189708ae79e9f4
Ross Parsons <rwp140@gmail.com>
2018-01-15 16:58:32 -0700

    added default error value [contacts controller]

--- src/components/Contacts/contacts.controller.js
+++ src/components/Contacts/contacts.controller.js
@@ -27,7 +27,11 @@ store.state.test = 'test';
   //Public
   var data = {};
   data.msg ='Welcome to contacts';
-  data.Contacts = [{Pid:'',Name:''}];
+  data.Contacts = [
+    {Pid:'contacts not loaded',Name:'contacts not loaded'},
+    {Pid:'contacts not loaded',Name:'if this persists please contact:(###)###-####'},
+    {Pid:'contacts not loaded',Name:'(###)###-#### or ####@####.com'}
+  ];
   // var Users = [''];
   //Private
 //Functions
