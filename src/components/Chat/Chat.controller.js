fd61c7add01298d20f71ca04c4021f0f51ffce48
Ross Parsons <rwp140@gmail.com>
2018-01-18 15:52:01 -0800

    organizational changes

--- src/components/Chat/Chat.controller.js
+++ src/components/Chat/Chat.controller.js
@@ -4,6 +4,28 @@ const actionSelection = require('./SubComponents/actionSelection').default;
 const chatTopBar = require('./SubComponents/chatTopBar').default;
 const store = require('../../store');
 
+//================================= Variables ==================================
+  //private
+  //public
+  var data = {};
+  // data.chatID = 0;
+  data.messages = [{SenderName:"System",content:"No messages yet.",direction:"right"}];
+  data.contacts = ["error name not saved to DB"];
+
+//================================= Function ===================================
+
+  //private
+  //public
+  function LoadChat(){
+      // data.chatID = store.state.chatID;
+      console.log(this.chatID);
+      let chat = svc.LoadChat(this.chatID);
+      if(chat){
+        data.messages = chat.messages;
+        data.contacts = chat.contacts;
+      }
+  }
+
 const svc = require('./Chat.service.js')
 //Exports
 module.exports = {
@@ -19,22 +41,3 @@ module.exports = {
   props:['chatID'],
   mounted:LoadChat
 }
-//Variables
-  //public
-  var data = {};
-  // data.chatID = 0;
-  data.messages = [{SenderName:"System",content:"No messages yet.",direction:"right"}];
-  data.contacts = ["error name not saved to DB"];
-  //private
-//Function
-  //public
-  function LoadChat(){
-      // data.chatID = store.state.chatID;
-      console.log(this.chatID);
-      let chat = svc.LoadChat(this.chatID);
-      if(chat){
-        data.messages = chat.messages;
-        data.contacts = chat.contacts;
-      }
-  }
-  //private
