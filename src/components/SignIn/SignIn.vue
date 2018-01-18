45c47e24078d247fb9ff6fdfe54b8a96da79603e
Ross William Parsons [140] <rwp140@gmail.com>
2018-01-17 21:24:31 -0700

    renamed files to better fit chosen conventions

--- src/components/SignIn/SignIn.vue
+++ src/components/SignIn/SignIn.vue
@@ -1,9 +1,9 @@
-<template src="./SignIn.html">
+<template src="./SignIn.template.html">
 </template>
 
 <script src='./SignIn.Controller.js'>
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style src='./style.css' scoped>
+<style src='./signIn.style.css' scoped>
 </style>
