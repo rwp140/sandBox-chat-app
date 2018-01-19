f39d2f6d03d47012f28e7e5e52cf3b729e96b0a9
Ross Parsons <rwp140@gmail.com>
2018-01-19 00:41:54 -0800

    should add hover effects and jump to empty profile page

--- src/components/Contacts/SubComponents/topBar.vue
+++ src/components/Contacts/SubComponents/topBar.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="top-Bar">
     <h1>contacts</h1>
-    profile >
+    <span id="profile-Link" v-on:click="loadProfile">profile ></span>
   </div>
 </template>
 
@@ -12,23 +12,38 @@ export default {
     return {
       msg: ''
     }
+  },
+  methods:{
+    loadProfile:function() {
+      this.$router.push({path:"/profile"})
+    }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1{
+.top-Bar h1{
   display: inline-block;
   height: inherit;
   margin-top: 0px;
   margin-bottom: 0px;
+  flex:auto;
+  align-self:center;
 }
 .top-Bar{
   border: black 1px solid;
   position: fixed;
   width: 100%;
   height: 3em;
-  /* background-color: purple; */
+  display: flex;
+  flex-direction:row;
+}
+#profile-Link{
+  align-self:flex-end;
+}
+#profile-Link :hover{
+  background-color: rgb(1,1,1);
+  background-color: rgba(0,0,0,0.2);
 }
 </style>
