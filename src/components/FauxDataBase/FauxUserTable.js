fd1a1cc6a246707f910c201ccff287b3020185b8
Ross Parsons <rwp140@gmail.com>
2018-01-21 01:55:18 -0700

    added another test user

--- src/components/FauxDataBase/FauxUserTable.js
+++ src/components/FauxDataBase/FauxUserTable.js
@@ -1,5 +1,6 @@
 export function data () {
   return [
-      {SignIN:"Anna@email.com",password:"Password",PID:72010154}
+      {SignIN:"Anna@email.com",password:"Password",PID:72010154},
+      {SignIN:"Lenna@email.com",password:"Password",PID:11992}
     ]
 }
