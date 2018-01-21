fbc9b7ce213b7c3e4e11f33c3e68edf2e4253b23
Ross Parsons <rwp140@gmail.com>
2018-01-21 02:16:03 -0700

    created fauxreadReceitpDatabase

---
+++ src/components/FauxDataBase/FauxReadRecieptDB.js
@@ -0,0 +1,18 @@
+//{
+//  [
+//    {
+//      chatID:String
+//      readReciept: [
+//        {Name:String,PID:String,readLine:number},
+//        {Name:String,PID:String,readLine:number},
+//      ]
+//    },
+//    {
+//      chatID:String
+//      readReciept: [
+//        {Name:String,PID:String,readLine:number},
+//        {Name:String,PID:String,readLine:number},
+//      ]
+//    }
+//  ]
+//}
