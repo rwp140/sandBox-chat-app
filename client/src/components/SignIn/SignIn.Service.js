20be28d1c533727a294b4a1f980fd519d5418b93
Ross Parsons <rwp140@gmail.com>
2018-01-30 00:26:54 -0700

    moved content to seperate server code and still proliferate to git

---
+++ client/src/components/SignIn/SignIn.Service.js
@@ -0,0 +1,42 @@
+//Imports
+// const Router = require('../.././router/index').default;
+const store = require('../../store');
+const FauxServer = require('../FauxDataBase/FauxServer');
+// const Server = require('../.././router/index');
+
+//Exports
+// module.exports = {
+//   SignInUserPost
+// }
+
+//Varaibles
+  //public
+  //private
+//Functions
+  //public
+  export function SignInUserPost(router,userData){
+    // console.log('signing in user');
+
+    FauxServer.get('/user',userData)
+      .then((_data)=>{
+        // console.log("promise");
+        console.log(_data);
+        // LoadContactsPage(router);
+        store.state.PID = _data.pid;
+        localStorage.PID = _data.pid;
+        localStorage.DisplayName = _data.DisplayName;
+
+        // console.log(store.state);
+        LoadContactsPage(router);
+         // _data;
+      }).catch((message)=>{
+        console.error(message);
+      });
+    // console.log(data);
+    // LoadContactsPage(router);
+  }
+  //private
+  export function LoadContactsPage(router) {
+
+    router.push({ path: '/contacts' });
+  }
