d56aa90dcc6ad450a8cfb33ca6317568fac08ea1
Ross William Parsons [140] <rwp140@gmail.com>
2018-02-01 03:27:44 -0700

    basic set up for chat controller

---
+++ Server/Controllers/chatController.js
@@ -0,0 +1,17 @@
+// ====================== IMPORTS ==============================================
+const chat = require('../Models/chat');
+
+
+//POST: create New chat
+
+
+//check for chats
+
+
+//GET: chat contacts
+
+
+//GET: chat messages
+
+
+//POST: new chat message
