1ece5a1e61b54271308c7cfbd7778e07cf74f5b5
Ross William Parsons [140] <rwp140@gmail.com>
2018-02-01 03:36:12 -0700

    create profile model

---
+++ Server/Models/profile.js
@@ -0,0 +1,14 @@
+//imports
+const mongoose = require('mongoose');
+
+//schema
+const profileSchema = new mongoose.Schema({
+  name: String,
+  PID: String,
+  IconIMG: String
+}, {});
+
+//export
+const Profile = mongoose.model('profile', profileSchema);
+
+ module.exports = Chat;
