fda6c2092afdba2a950a91c2bd1812faa9ce8088
Ross William Parsons [140] <rwp140@gmail.com>
2018-02-01 03:27:34 -0700

    updated test routes

--- Server/routes.js
+++ Server/routes.js
@@ -1,5 +1,7 @@
+// ========================= IMPORTS ===========================================
 const express = require('express')
 var router = express.Router()
+// ====================== Controllers ==========================================
 
 // middleware that is specific to this router
 router.use(function timeLog (req, res, next) {
@@ -12,13 +14,24 @@ router.get('/', function (req, res) {
 })
 // define the about api route
 router.get('/about', function (req, res) {
+  console.log('===== ===== About the chat api ===== =====');
+  console.log('call /test to test "req" and "res" data.');
   res.send('About the chat api')
 })
 // define the test page route
 router.get('/Test', function (req, res) {
   let message = ['Test Data is as Follows',req,res];
+  console.log('Test Data is as Follows');
+  console.log(req);
+  console.log(res);
   res.send(message)
 })
-
+router.post('/Test', function (req, res) {
+  let message = ['Test Data is as Follows',req,res];
+  console.log('Test Data is as Follows');
+  console.log(req);
+  console.log(res);
+  res.send(message)
+})
 
 module.exports = router
