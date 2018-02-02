27d45ed1a20d5693ddc55f3c988b65b839f889f8
Ross Parsons <rwp140@gmail.com>
2018-02-02 15:27:50 -0700

    set up user controller

--- Server/Controllers/UserController.js
+++ Server/Controllers/UserController.js
@@ -1,7 +1,20 @@
 //IMPORTS
 
 //POST: new user requires admin token
+exports.CreateNewUser = (req,reg,next) =>{
 
+}
 //POST: sign in user
+exports.SignInUser = (req,reg,next) =>{
 
-//GET: get PID
+}
+
+//GET: get PID <redundancy>
+exports.GetPID = (req,reg,next) =>{
+
+}
+//GET: Send Forgot Password Form
+
+//GET: Send Verification Request
+
+//verification check
