8be63f92847a404874d28c0b6f8342419bd82d45
Ross Parsons <rwp140@gmail.com>
2018-02-02 15:27:50 -0700

    set up user controller

--- Server/Controllers/UserController.js
+++ Server/Controllers/UserController.js
@@ -1,7 +1,48 @@
 //IMPORTS
 
 //POST: new user requires admin token
-
+exports.CreateNewUser = (req,reg,next) =>{
+//expects
+  //username
+  //token
+}
 //POST: sign in user
+exports.SignInUser = (req,reg,next) =>{
 
-//GET: get PID
+}
+//POST: Register Verification
+exports.RegisterVerification = (req,reg,next) =>{
+
+}
+
+//POST: Update Password
+exports.UpdatePassword = (req,reg,next) =>{
+
+}
+
+//GET: get PID <redundancy>
+exports.GetPID = (req,reg,next) =>{
+
+}
+//GET: Send Forgot User Name
+exports.SendForgotUserNameRequest = (req,reg,next) =>{
+
+}
+//GET: Send Forgot Password Request
+exports.SendForgotPasswordRequest = (req,reg,next) =>{
+
+}
+
+//GET: Send Verification Request
+exports.SendVerificationRequest = (req,reg,next) =>{
+
+}
+
+//Send Verification Request function
+function SendVerificationRequestFunction() {
+
+}
+//verification check
+function VerificationCheck() {
+
+}
