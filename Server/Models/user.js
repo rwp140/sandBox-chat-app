bded415992b8a16d19bd6cdd81758a863bbc4917
Ross William Parsons [140] <rwp140@gmail.com>
2018-02-03 01:13:35 -0700

    updated user model

--- Server/Models/user.js
+++ Server/Models/user.js
@@ -6,9 +6,37 @@ const userprofileSchema = new mongoose.Schema({
   userName: String,
   password: String,
   PID: String,
-  UserToken: String
+  UserToken: String,
+  verified: Bool
 }, {});
 
+/**
+  * Password hash middleware.
+  */
+userSchema.pre('save', function save(next) {
+ const user = this;
+
+ if (!user.isModified('password')) { return next(); }
+ bcrypt.genSalt(10, (err, salt) => {
+   if (err) { return next(err); }
+   bcrypt.hash(user.password, salt, null, (err, hash) => {
+     if (err) { return next(err); }
+     user.password = hash
+
+     next();
+   });
+ });
+});
+
+/**
+  * Helper method for validating user's password.
+  */
+
+userSchema.methods.comparePassword = function comparePassword(candidatePassword, cb) {
+ bcrypt.compare(candidatePassword, this.password, (err, isMatch) => {
+   cb(err, isMatch);
+ });
+};
 //export
 const Profile = mongoose.model('profile', profileSchema);
 
